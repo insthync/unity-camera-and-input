6377fdfa5363e902e27d9d8d2b4ef279f8027dcf
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-10-05 22:09:39 +0700

    Add smooth input controls codes

--- Scripts/Camera/FollowCameraControls.cs
+++ Scripts/Camera/FollowCameraControls.cs
@@ -8,28 +8,48 @@ public class FollowCameraControls : FollowCamera
     public bool updateRotationX = false;
     public bool updateRotationY = false;
     public bool updateZoom = true;
+
     [Header("X Rotation")]
     public bool limitXRotation;
+    [Range(-360, 360)]
     public float minXRotation = 0;
+    [Range(-360, 360)]
     public float maxXRotation = 0;
+    public bool smoothRotateX;
+    public float rotateXSmoothing = 10.0f;
+
     [Header("Y Rotation")]
     public bool limitYRotation;
+    [Range(-360, 360)]
     public float minYRotation = 0;
+    [Range(-360, 360)]
     public float maxYRotation = 0;
+    public bool smoothRotateY;
+    public float rotateYSmoothing = 10.0f;
+
     [Header("General Rotation Settings")]
     public float startXRotation;
     public float startYRotation;
     public float rotationSpeed = 5;
+
     [Header("Zoom")]
     public bool limitZoomDistance;
     public float minZoomDistance;
     public float maxZoomDistance;
+    public bool smoothZoom;
+    public float zoomSmoothing = 10.0f;
+
     [Header("General Zoom Settings")]
     public float startZoomDistance;
     public float zoomSpeed = 5;
+
     [Header("Save Camera")]
     public bool isSaveCamera;
     public string savePrefsPrefix = "GAMEPLAY";
+
+    private float xVelocity;
+    private float yVelocity;
+    private float zoomVelocity;
 
     private void Start()
     {
@@ -55,28 +75,57 @@ public class FollowCameraControls : FollowCamera
             PlayerPrefs.Save();
         }
 
+        float deltaTime = Time.deltaTime;
+        // X rotation
         if (updateRotation || updateRotationX)
-        {
-            float mY = InputManager.GetAxis("Mouse Y", false);
-            xRotation -= mY * rotationSpeed;
-            if (limitXRotation)
-                xRotation = Mathf.Clamp(xRotation, minXRotation, maxXRotation);
-        }
+            xVelocity += InputManager.GetAxis("Mouse Y", false) * rotationSpeed;
+        xRotation -= xVelocity;
+        if (limitXRotation)
+            xRotation = ClampAngleBetweenMinAndMax(xRotation, minXRotation, maxXRotation);
+        else
+            xRotation = ClampAngleBetweenMinAndMax(xRotation, -360, 360);
 
+        // Y rotation
         if (updateRotation || updateRotationY)
-        {
-            float mX = InputManager.GetAxis("Mouse X", false);
-            yRotation += mX * rotationSpeed;
-            if (limitYRotation)
-                yRotation = Mathf.Clamp(yRotation, minYRotation, maxYRotation);
-        }
+            yVelocity += InputManager.GetAxis("Mouse X", false) * rotationSpeed;
+        yRotation += yVelocity;
+        if (limitYRotation)
+            yRotation = ClampAngleBetweenMinAndMax(yRotation, minYRotation, maxYRotation);
+        else
+            yRotation = ClampAngleBetweenMinAndMax(yRotation, -360, 360);
 
+        // Zoom
         if (updateZoom)
-        {
-            float mZ = InputManager.GetAxis("Mouse ScrollWheel", false);
-            zoomDistance += mZ * zoomSpeed;
-            if (limitZoomDistance)
-                zoomDistance = Mathf.Clamp(zoomDistance, minZoomDistance, maxZoomDistance);
-        }
+            zoomVelocity += InputManager.GetAxis("Mouse ScrollWheel", false) * zoomSpeed;
+        zoomDistance += zoomVelocity;
+        if (limitZoomDistance)
+            zoomDistance = Mathf.Clamp(zoomDistance, minZoomDistance, maxZoomDistance);
+
+        // X rotation smooth
+        if (smoothRotateX)
+            xVelocity = Mathf.Lerp(xVelocity, 0, deltaTime * rotateXSmoothing);
+        else
+            xVelocity = 0f;
+
+        // Y rotation smooth
+        if (smoothRotateY)
+            yVelocity = Mathf.Lerp(yVelocity, 0, deltaTime * rotateYSmoothing);
+        else
+            yVelocity = 0f;
+
+        // Zoom smooth
+        if (smoothZoom)
+            zoomVelocity = Mathf.Lerp(zoomVelocity, 0, deltaTime * zoomSmoothing);
+        else
+            zoomVelocity = 0f;
+    }
+
+    private float ClampAngleBetweenMinAndMax(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
     }
 }
