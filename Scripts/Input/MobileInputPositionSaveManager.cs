86784e94e4fb507348c0ca3f3dc98cfa7853f2e7
Insthync <ittipon.bay@gmail.com>
2020-08-06 17:24:29 +0700

    Add simple position save manager, may set button to save changes or reset by its functions.

---
+++ Scripts/Input/MobileInputPositionSaveManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobileInputPositionSaveManager : MonoBehaviour
+{
+    public void TurnOnEditMode()
+    {
+        var comps = GetComponentsInChildren<MobileInputPositionSave>();
+        foreach (var comp in comps)
+        {
+            comp.isEditMode = true;
+        }
+    }
+
+    public void TurnOffEditMode()
+    {
+        var comps = GetComponentsInChildren<MobileInputPositionSave>();
+        foreach (var comp in comps)
+        {
+            comp.isEditMode = false;
+        }
+    }
+
+    public void SavePositions()
+    {
+        var comps = GetComponentsInChildren<MobileInputPositionSave>();
+        foreach (var comp in comps)
+        {
+            comp.SavePosition();
+        }
+    }
+
+    public void ResetPositions()
+    {
+        var comps = GetComponentsInChildren<MobileInputPositionSave>();
+        foreach (var comp in comps)
+        {
+            comp.ResetPosition();
+        }
+    }
+}
