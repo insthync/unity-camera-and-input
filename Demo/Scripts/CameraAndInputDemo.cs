06f822a4db15c160daf9ae8c141b496145795fd8
Insthync <ittipon.bay@gmail.com>
2020-08-07 17:39:00 +0700

    Remake the demo

---
+++ Demo/Scripts/CameraAndInputDemo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAndInputDemo : MonoBehaviour
+{
+    public CharacterController character;
+    public Camera controlleringCamera;
+    public float moveSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InputManager.useMobileInputOnNonMobile = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var inputV = InputManager.GetAxis("Vertical", false);
+        var inputH = InputManager.GetAxis("Horizontal", false);
+        var moveDirection = Vector3.zero;
+        moveDirection += controlleringCamera.transform.forward * inputV;
+        moveDirection += controlleringCamera.transform.right * inputH;
+        character.SimpleMove(moveDirection * moveSpeed * 1000f * Time.deltaTime);
+    }
+}
