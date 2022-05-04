964221c221fa5b28f98215b6d4afb09802ea26fc
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-05-04 15:36:18 +0700

    Add mobile input toggle by key code component

---
+++ Scripts/Input/MobileInputToggleByKeyCode.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MobileInputToggleByKeyCode : BaseMobileInputToggle
+{
+    [SerializeField]
+    private KeyCode keyCode = KeyCode.None;
+
+    protected override void OnToggle(bool isOn)
+    {
+        if (isOn)
+            InputManager.SetKeyDown(keyCode);
+        else
+            InputManager.SetKeyUp(keyCode);
+    }
+}
