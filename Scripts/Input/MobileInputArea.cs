94b40d02d0d4eb32fe94783dd963ed91d5a50bf9
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2023-03-31 19:12:52 +0700

    feat: add `MobileInputArea` tag component

---
+++ Scripts/Input/MobileInputArea.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+/// <summary>
+/// Empty component, use it as a tag for mobile input touching detecting
+/// </summary>
+public class MobileInputArea : MonoBehaviour, IMobileInputArea
+{
+}
